b8b89663fc80625dffff3b07b43ead54044628e7
Matheus Trindade <trindadematheus27@gmail.com>
2019-12-13 00:46:06 -0300

    layout, estilos e funcionalidade de contagem regressiva

---
+++ desktop/src/js/counter.js
@@ -0,0 +1,45 @@
+document.querySelector("#playBtn").addEventListener("click", () => {
+  counter();
+});
+
+function counter() {
+  let minutes = 5;
+  let seconds = 0;
+
+  let TurnCountdown = setInterval(() => {
+    let display = document.querySelector("#playBtn");
+
+    playBtn.classList.remove("centerPlay");
+    playBtn.classList.add("centerCount");
+
+    display.innerHTML = verifyMinutes(minutes, seconds);
+    seconds--;
+
+    if (seconds == -1 && minutes == 0) {
+      display.innerHTML = ">";
+      playBtn.classList.remove("centerCount");
+      playBtn.classList.add("centerPlay");
+
+      clearInterval(TurnCountdown);
+    }
+    if (seconds <= -1) {
+      minutes--;
+      seconds = 59;
+    }
+  }, 1000);
+
+  function verifyMinutes(minutes, seconds) {
+    if (minutes < 10 && seconds >= 10) {
+      return (minutesSeconds = `0${minutes}:${seconds}`);
+    }
+    if (minutes >= 10 && seconds < 10) {
+      return (minutesSeconds = `${minutes}:0${seconds}`);
+    }
+    if (minutes >= 10 && seconds >= 10) {
+      return (minutesSeconds = `${minutes}:${seconds}`);
+    }
+    if (minutes < 10 && seconds < 10) {
+      return (minutesSeconds = `0${minutes}:0${seconds}`);
+    }
+  }
+}
